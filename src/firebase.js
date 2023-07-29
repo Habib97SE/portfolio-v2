520b6bdb05ed7ae83c61d7c3aefbc3bba86ce381
Habib Hezarehee <71149841+Habib97SE@users.noreply.github.com>
2023-07-29 13:10:22 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import { getStorage } from 'firebase/storage'
 import { getFirestore } from 'firebase/firestore/lite'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBJrQxSxP3Q2mbjDWRPZAnGxawz6mVkJFo',
-  authDomain: 'civil-density-286221.firebaseapp.com',
-  databaseURL: 'https://civil-density-286221.firebaseio.com',
-  projectId: 'civil-density-286221',
-  storageBucket: 'civil-density-286221.appspot.com',
-  messagingSenderId: '367214077560',
-  appId: '1:367214077560:web:ed544556c81331df4daf8d',
-}
+  apiKey: "AIzaSyBJrQxSxP3Q2mbjDWRPZAnGxawz6mVkJFo",
+  authDomain: "civil-density-286221.firebaseapp.com",
+  databaseURL: "https://civil-density-286221.firebaseio.com",
+  projectId: "civil-density-286221",
+  storageBucket: "civil-density-286221.appspot.com",
+  messagingSenderId: "367214077560",
+  appId: "1:367214077560:web:ed544556c81331df4daf8d"
+};
 
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth()
